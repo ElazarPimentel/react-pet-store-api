88de8420536c1470fabbb14bc9ae72315513eccd
Elazar Pimentel <elazar.pimentel@gmail.com>
2024-11-13 20:30:22 -0300

    Moviendo mongoDB a la carpeta del projecto

--- models/product.model.js
+++ models/product.model.js
@@ -1,15 +1,16 @@
-// Filename: models/product.model.js
+// models/product.model.js
 // Alumno: Alessio (Elazar) Aguirre Pimentel
 
 import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
-  code: { type: String, required: true, unique: true },
   price: { type: Number, required: true },
-  stock: { type: Number, required: true },
-  category: { type: String, required: true },
+  thumbnail: { type: String, required: true },
+  description: { type: String, default: '' },
+  code: { type: String, default: '' },
+  stock: { type: Number, default: 0 },
+  category: { type: String, default: '' },
   status: { type: Boolean, default: true },
   thumbnails: { type: [String], default: [] },
 });
--- public/index.js
+++ public/index.js
@@ -1,22 +1,56 @@
 // public/index.js
 // Alumno: Alessio (Elazar) Aguirre Pimentel
 
-async function addToCart(productId) {
+async function getCartId() { // Aprendido a las malas
+    let cartId = localStorage.getItem('cartId');
+
+  if (!cartId) {
     try {
-      const response = await fetch(`/carts/add/${productId}`, {
+      const response = await fetch('/api/carts', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
       });
+
       const data = await response.json();
-      if (data.error) {
-        console.error('Error:', data.error);
-        alert('Hubo un problema al agregar el producto al carrito');
+
+      if (data._id) {
+        cartId = data._id;
+        localStorage.setItem('cartId', cartId);
       } else {
-        alert('Producto agregado al carrito');
+        console.error('Error al crear el carrito:', data);
       }
     } catch (error) {
-      console.error('Error:', error);
-      alert('Error de red al agregar el producto al carrito');
+      console.error('Error de red al crear el carrito:', error);
     }
   }
-  
+
+  return cartId;
+}
+
+async function addToCart(productId) {
+  try {
+    const cartId = await getCartId();
+
+    if (!cartId) {
+      alert('No se pudo obtener el ID del carrito.');
+      return;
+    }
+
+    const response = await fetch(`/api/carts/${cartId}/product/${productId}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const data = await response.json();
+
+    if (data.error) {
+      console.error('Error:', data.error);
+      alert('Hubo un problema al agregar el producto al carrito');
+    } else {
+      alert('Producto agregado al carrito');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    alert('Error de red al agregar el producto al carrito');
+  }
+}
